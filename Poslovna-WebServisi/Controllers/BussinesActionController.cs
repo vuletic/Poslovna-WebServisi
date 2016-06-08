a96b08c64ae0cfc5a6b415559fcf448dbf292df7
vuletic <vuletic93@gmail.com>
2016-06-08 23:11:11 +0200

    izvestaji za robnu karticu

--- Poslovna-WebServisi/Controllers/BussinesActionController.cs
+++ Poslovna-WebServisi/Controllers/BussinesActionController.cs
@@ -124,6 +124,12 @@ namespace WebAPI.Controllers
                 razmak += 20;
             }
 
+            gfx.DrawLine(XPens.Black, 20, 120 + razmak, page.Width - 20, 120 + razmak);
+
+            gfx.DrawString("Ukupna vrednost: " + pd.Ukupna_vrednost_Prijemni_dokument, font, XBrushes.Black,
+                  new XRect(20, 130 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
             // Save the document...
         
             MemoryStream stream = new MemoryStream();
@@ -147,8 +153,12 @@ namespace WebAPI.Controllers
             if (!handler.CheckToken(Request.Headers.Authorization.ToString()))
                 return null;
 
+            Robna_kartica rk = (from r in db.Robna_kartica.Include("Roba.Jedinica_mere").Include("Poslovna_godina").Include("Magacin").Include("Analitika_magacinske_kartice")
+                                    where r.Id_Robna_kartica == id
+                                    select r).FirstOrDefault();
+
             PdfDocument document = new PdfDocument();
-            document.Info.Title = "Created with PDFsharp";
+            //document.Info.Title = "Created with PDFsharp";
 
             // Create an empty page
             PdfPage page = document.AddPage();
@@ -157,18 +167,130 @@ namespace WebAPI.Controllers
             XGraphics gfx = XGraphics.FromPdfPage(page);
 
             // Create a font
-            XFont font = new XFont("Verdana", 20, XFontStyle.BoldItalic);
+            XFont font = new XFont("Verdana", 16, XFontStyle.Regular);
 
-            // Draw the text
-            gfx.DrawString("Hello, World!", font, XBrushes.Black,
-              new XRect(0, 0, page.Width, page.Height),
+            // Draw the text 
+            gfx.DrawString("Robna kartica", font, XBrushes.Black,
+              new XRect(0, 15, page.Width, page.Height / 8),
               XStringFormats.TopCenter);
 
-            font = new XFont("Verdana", 12, XFontStyle.Regular);
+            font = new XFont("Verdana", 10, XFontStyle.Regular);
 
-            gfx.DrawString("Hello, Worasdld!", font, XBrushes.Black,
-              new XRect(0, 0, page.Width, page.Height),
+            gfx.DrawString("Skladište: " + rk.Magacin.Naziv_Magacin, font, XBrushes.Black,
+              new XRect(20, 40, page.Width, page.Height / 8),
               XStringFormats.TopLeft);
+
+            gfx.DrawString("Roba: " + rk.Roba.Naziv_Roba, font, XBrushes.Black,
+              new XRect(20, 55, page.Width, page.Height / 8),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Poslovna godina: " + rk.Poslovna_godina.Godina_Poslovna_godina, font, XBrushes.Black,
+              new XRect(page.Width/2 + 100, 40, page.Width, page.Height / 8),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Jedinica mere: " + rk.Roba.Jedinica_mere.Oznaka_Jedinica_mere, font, XBrushes.Black,
+              new XRect(page.Width / 2 + 100, 55, page.Width, page.Height / 8),
+              XStringFormats.TopLeft);
+
+            gfx.DrawLine(XPens.Black, 20, 105, page.Width - 20, 105);
+
+            font = new XFont("Verdana", 8, XFontStyle.Regular);
+
+            gfx.DrawString("Rbr.", font, XBrushes.Black,
+              new XRect(20, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Vrsta", font, XBrushes.Black,
+              new XRect(60, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Smer", font, XBrushes.Black,
+              new XRect(120, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Količina", font, XBrushes.Black,
+              new XRect(160, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Jedinična cena", font, XBrushes.Black,
+              new XRect(250, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Vrednost", font, XBrushes.Black,
+              new XRect(340, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+            gfx.DrawString("Stanje", font, XBrushes.Black,
+              new XRect(430, 90, page.Width, page.Height),
+              XStringFormats.TopLeft);
+
+
+            int razmak = 0;
+
+            var an = rk.Analitika_magacinske_kartice.OrderBy(a => a.Redni_broj_Analitika_magacinske_kartice);
+
+            foreach (Analitika_magacinske_kartice a in an)
+            {
+                gfx.DrawString(a.Redni_broj_Analitika_magacinske_kartice.ToString(), font, XBrushes.Black,
+                  new XRect(20, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Vrsta_prometa_Analitika_magacinske_kartice, font, XBrushes.Black,
+                  new XRect(60, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Smer_Analitika_magacinske_kartice, font, XBrushes.Black,
+                  new XRect(120, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Kolicina_Analitika_magacinske_kartice.ToString(), font, XBrushes.Black,
+                  new XRect(160, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Cena_Analitika_magacinske_kartice.ToString(), font, XBrushes.Black,
+                  new XRect(250, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Vrednost_Analitika_magacinske_kartice.ToString(), font, XBrushes.Black,
+                  new XRect(340, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                gfx.DrawString(a.Ukupno_stanje_Analitika_magacinske_kartice.ToString(), font, XBrushes.Black,
+                  new XRect(430, 115 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+                razmak += 15;
+            }
+
+            gfx.DrawLine(XPens.Black, 20, 115 + razmak, page.Width - 20, 115 + razmak);
+
+            gfx.DrawString("Prosečna cena: " + rk.Prosecna_cena_Robna_kartica, font, XBrushes.Black,
+                  new XRect(20, 125 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Promet ulaza količinski: " + rk.Promet_ulaza_kol_Robna_kartica, font, XBrushes.Black,
+                  new XRect(20, 140 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Promet izlaza količinski: " + rk.Promet_izlaza_kol_Robna_kartica, font, XBrushes.Black,
+                  new XRect(20, 155 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Ukupna količina: " + rk.Ukupna_kolicina_Robna_kartica, font, XBrushes.Black,
+                  new XRect(20, 170 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Promet ulaza vrednosno: " + rk.Promet_ulaza_vr_Robna_kartica, font, XBrushes.Black,
+                  new XRect(page.Width / 2 + 100, 140 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Promet izlaza vrednosno: " + rk.Promet_izlaza_vr_Robna_kartica, font, XBrushes.Black,
+                  new XRect(page.Width / 2 + 100, 155 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
+
+            gfx.DrawString("Ukupna vrednost: " + rk.Ukupna_vrednost_Robna_kartica, font, XBrushes.Black,
+                  new XRect(page.Width / 2 + 100, 170 + razmak, page.Width, page.Height),
+                  XStringFormats.TopLeft);
 
             // Save the document...
 
